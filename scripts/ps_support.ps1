690e6529c41340a1450f9e713c037ec7515ce264
Javier Carnero <jcarnero@plainconcepts.com>
2022-09-26 11:44:30 +0200

    CD pipeline

---
+++ scripts/ps_support.ps1
@@ -0,0 +1,9 @@
+Set-StrictMode -Version Latest
+$ErrorActionPreference = "Stop"
+$PSDefaultParameterValues['*:ErrorAction'] = 'Stop'
+function ThrowOnNativeFailure {
+    if (-not $?) {
+        Pop-Location
+        throw 'Native Failure'
+    }
+}
